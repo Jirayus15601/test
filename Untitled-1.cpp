553445144270f75c2721a3e0bfd281f0f25a0127
Jirayus <jirayus1605@gmail.com>
2019-01-14 17:18:23 +0700

    ex3-2.3

--- Untitled-1.cpp
+++ Untitled-1.cpp
@@ -1,1 +1,20 @@
-#include<iostram>
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int a;
+    int b;
+    int c;
+    cout << "Enter first number :  ";
+    cin >> a;
+    cout << "Enter last number : ";
+    cin >> b;
+    c=0;
+    while (a*a <= b*b){
+    	c=(a*a)+c;
+    	a++;
+	}
+	cout << c;
+return 0;
+}
